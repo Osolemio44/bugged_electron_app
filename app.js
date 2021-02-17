46939a913f1efec2d644c9953d31447e89a94399
preantas <tranquiloteov@gmail.com>
2021-02-17 13:19:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow() {
+
+    mainWindow = new BrowserWindow({
+        width: 1000, height: 800,        
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        },
+        //icon:"assets/imgs/icon.ico"
+        icon:path.join(__dirname,"assets/imgs/icon.ico")
+    })
+
+    // Load index.html into the new BrowserWindow
+    //mainWindow.loadFile('./views/main.html')
+    mainWindow.loadFile(path.join(__dirname,'views/main.html'));
+
+    // Open DevTools - Remove for PRODUCTION!
+    mainWindow.webContents.openDevTools();     // SOLO PARA DESARROLLO
+    mainWindow.setMenuBarVisibility(false)
+    // Listen for window being closed
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+    if (mainWindow === null) createWindow()
+})
